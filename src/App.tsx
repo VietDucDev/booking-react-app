a8d2b7fce74a7c6a6678b68fed268ee7e9e1c31b
VietDucDev <vietducdo00@gmail.com>
2024-04-09 22:14:53 +0700

    scss configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import { Fragment, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import HotelListPage from "./components/HotelListPage";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log("aaa");
-  console.log("bbb");
-  console.log("test push from TUNG");
-  console.log("vai ca lz");
-
   return (
     <Fragment>
       <HotelListPage />
--- src/components/HotelListPage.tsx
+++ src/components/HotelListPage.tsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import "../style/_hotelListPage.scss";
 
 const HotelListPage = () => {
   return (
