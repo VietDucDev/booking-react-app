fe6189607794c97a79f505bc17181746cecda85b
nstdev1001 <sontung2000ct@gmail.com>
2024-04-09 15:52:30 +0700

    add folder sass and compile to css

---
+++ src/components/HotelListPage.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const HotelListPage = () => {
+  return (
+    <Fragment>
+      <div className="main-container hotel_container">
+        <h1 className="testH1">test hotel</h1>
+      </div>
+    </Fragment>
+  );
+};
+
+export default HotelListPage;
