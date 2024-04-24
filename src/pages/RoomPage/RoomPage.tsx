6b24892d98adc2a4fbe6aedb872cc0adb69f391f
VietDucDev <vietducdo00@gmail.com>
2024-04-24 18:53:31 +0700

    update

--- src/pages/RoomPage/RoomPage.tsx
+++ src/pages/RoomPage/RoomPage.tsx
@@ -243,11 +243,11 @@ const RoomPage = () => {
                               fill="none"
                               viewBox="0 0 24 24"
                               stroke="currentColor"
-                              stroke-width="4"
+                              strokeWidth="4"
                             >
                               <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                                 d="M14 5l7 7m0 0l-7 7m7-7H3"
                               ></path>
                             </svg>
