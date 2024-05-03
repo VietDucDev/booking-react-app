0772fdc75c6eacea1c7ba02a5261835f3423303d
Doan Van Hoang <doavanhoang060902@gmail.com>
2024-05-03 20:05:59 +0700

    change hero.tsx

--- src/pages/HomePage/Hero.tsx
+++ src/pages/HomePage/Hero.tsx
@@ -61,22 +61,6 @@ const Hero = () => {
     navigate(`/hotel-list?district_name=${location}`);
   };
 
-  const date = new Date();
-
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-
-  const formattedDate = `${hours.toString().padStart(2, "0")}h${minutes
-    .toString()
-    .padStart(2, "0")} - ${day.toString().padStart(2, "0")}/${month
-    .toString()
-    .padStart(2, "0")}/${year}`;
-
-  console.log(formattedDate);
-
   const handleScroll = () => {
     const searchBar = document.querySelector(".search_bar");
     if (searchBar) {
